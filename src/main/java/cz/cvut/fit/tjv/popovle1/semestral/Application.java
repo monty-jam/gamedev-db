745506b5b9e27a3bb7212c4725d9a581de036a69
popovle1 <popovle1@fit.cvut.cz>
2021-11-03 20:51:54 +0100

    Spring and MySQL are connected, added full implementation of CRUD for Developer table.

---
+++ src/main/java/cz/cvut/fit/tjv/popovle1/semestral/Application.java
@@ -0,0 +1,12 @@
+package cz.cvut.fit.tjv.popovle1.semestral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
--- src/main/java/cz/cvut/fit/tjv/popovle1/semestral/server.java
+++
@@ -1,4 +0,0 @@
-package cz.cvut.fit.tjv.popovle1.semestral;
-
-public class server {
-}
