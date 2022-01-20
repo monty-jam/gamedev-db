e90c71fb1428f565c10c7d1a4019e0d6805a4060
popovle1 <popovle1@fit.cvut.cz>
2022-01-21 00:43:18 +0100

    Creating test files.

---
+++ src/test/java/cz/cvut/fit/tjv/popovle1/semestral/crud_gamedev/controller/GameControllerTest.java
@@ -0,0 +1,7 @@
+package cz.cvut.fit.tjv.popovle1.semestral.crud_gamedev.controller;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GameControllerTest {
+}
