6db141338d56b348c10a7e6fef42da272a69f213
popovle1 <popovle1@fit.cvut.cz>
2022-01-21 14:08:49 +0100

    Added updateTest and updateWithStudioTest for DevServiceTest

--- src/test/java/cz/cvut/fit/tjv/popovle1/semestral/crud_gamedev/service/DevServiceTest.java
+++ src/test/java/cz/cvut/fit/tjv/popovle1/semestral/crud_gamedev/service/DevServiceTest.java
@@ -34,12 +34,14 @@ public class DevServiceTest {
 
     // For tests without studio
     private final Dev dev1 = new Dev("John", "Carmack", "Programmer", null);
-    private final DevDTO devDTO1 = new DevDTO(null, "Michel", "Ancel", "Art Designer", null);
+    private final DevDTO devDTO1 = new DevDTO(null, "John", "Carmack", "Programmer", null);
+    private final Dev newDev1 = new Dev("Michel", "Ancel", "Art Designer", null);
     private final DevDTO newDevDTO1 = new DevDTO(null, "Michel", "Ancel", "Art Designer", null);
 
     // For tests with studio
     private final Dev dev2 = new Dev("John", "Carmack", "Programmer", studio);
     private final DevDTO devDTO2 = new DevDTO(null, "John", "Carmack", "Programmer", 1);
+    private final Dev newDev2 = new Dev("Michel", "Ancel", "Art Designer", newStudio);
     private final DevDTO newDevDTO2 = new DevDTO(null, "Michel", "Ancel", "Art Designer", 2);
 
     @Test
@@ -57,7 +59,7 @@ public class DevServiceTest {
 
     @Test
     void createWithStudioTest() throws Exception {
-        BDDMockito.given(studioRepo.findById(1)).willReturn(Optional.of(studio));
+        BDDMockito.given(studioRepo.findById(studio.getId())).willReturn(Optional.of(studio));
         BDDMockito.given(devRepo.save(any(Dev.class))).willReturn(dev2);
 
         DevDTO retDev = devService.create(devDTO2);
@@ -67,22 +69,22 @@ public class DevServiceTest {
         Assertions.assertEquals(retDev.getSpecialization(), devDTO2.getSpecialization());
         Assertions.assertEquals(retDev.getStudioId(), devDTO2.getStudioId());
 
-        Mockito.verify(studioRepo, Mockito.times(1)).findById(1);
+        Mockito.verify(studioRepo, Mockito.times(1)).findById(studio.getId());
         Mockito.verify(devRepo, Mockito.times(1)).save(any(Dev.class));
     }
 
     @Test
     void readTest() throws Exception {
-        BDDMockito.given(devRepo.findById(1)).willReturn(Optional.of(dev2));
+        BDDMockito.given(devRepo.findById(dev2.getId())).willReturn(Optional.of(dev2));
 
-        DevDTO retDev = devService.read(1);
+        DevDTO retDev = devService.read(dev2.getId());
 
         Assertions.assertEquals(retDev.getName(), devDTO2.getName());
         Assertions.assertEquals(retDev.getSurname(), devDTO2.getSurname());
         Assertions.assertEquals(retDev.getSpecialization(), devDTO2.getSpecialization());
         Assertions.assertEquals(retDev.getStudioId(), devDTO2.getStudioId());
 
-        Mockito.verify(devRepo, Mockito.times(2)).findById(1);
+        Mockito.verify(devRepo, Mockito.times(2)).findById(dev2.getId());
     }
 
     @Test
@@ -95,6 +97,40 @@ public class DevServiceTest {
 
         Assertions.assertEquals(retDevs.size(), 2);
         Mockito.verify(devRepo, Mockito.times(1)).findAll();
+    }
+
+    @Test
+    void updateTest() throws Exception {
+        BDDMockito.given(devRepo.findById(dev1.getId())).willReturn(Optional.of(dev1));
+        BDDMockito.given(devRepo.save(any(Dev.class))).willReturn(newDev1);
+
+        DevDTO retDev = devService.update(newDevDTO1, dev1.getId());
+
+        Assertions.assertEquals(retDev.getName(), newDevDTO1.getName());
+        Assertions.assertEquals(retDev.getSurname(), newDevDTO1.getSurname());
+        Assertions.assertEquals(retDev.getSpecialization(), newDevDTO1.getSpecialization());
+        Assertions.assertEquals(retDev.getStudioId(), newDevDTO1.getStudioId());
+
+        Mockito.verify(devRepo, Mockito.times(2)).findById(dev1.getId());
+        Mockito.verify(devRepo, Mockito.times(1)).save(any(Dev.class));
+    }
+
+    @Test
+    void updateWithStudioTest() throws Exception {
+        BDDMockito.given(devRepo.findById(dev2.getId())).willReturn(Optional.of(dev2));
+        BDDMockito.given(studioRepo.findById(newStudio.getId())).willReturn(Optional.of(newStudio));
+        BDDMockito.given(devRepo.save(any(Dev.class))).willReturn(newDev2);
+
+        DevDTO retDev = devService.update(newDevDTO2, dev2.getId());
+
+        Assertions.assertEquals(retDev.getName(), newDevDTO2.getName());
+        Assertions.assertEquals(retDev.getSurname(), newDevDTO2.getSurname());
+        Assertions.assertEquals(retDev.getSpecialization(), newDevDTO2.getSpecialization());
+        Assertions.assertEquals(retDev.getStudioId(), newDevDTO2.getStudioId());
+
+        Mockito.verify(devRepo, Mockito.times(2)).findById(dev2.getId());
+        Mockito.verify(studioRepo, Mockito.times(1)).findById(newStudio.getId());
+        Mockito.verify(devRepo, Mockito.times(1)).save(any(Dev.class));
     }
 
     @Test
